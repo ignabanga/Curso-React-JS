5a899beee6ba0854b37db633442bc36aaa666d02
Sebastian Banga <sebasbanga@gmail.com>
2022-09-14 22:55:20 -0300

    commit -m "Eliminar productos, carrito"

---
+++ src/components/Cart/CartContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CartList from './CartList';
+import CartResume from './CartResume';
+
+const CartContainer = () => {
+    
+    return (
+        <>
+            <CartResume/>
+            <CartList/>
+        </>
+    );
+}
+
+export default CartContainer;
